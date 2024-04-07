d9819de8f719f1c4c2a20e7f0b5b830a712a7e09
Mirzakhan Mirzakhanov <mirzakhan.mirzakhanov@avtodom.ru>
2024-04-08 00:03:34 +0300

    feat: cleaned project

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import './assets/main.css'
+import '@/assets/styles/css/_reset.css'
+import '@/assets/styles/css/_global.css'
+import '@/assets/styles/scss/_fonts.scss'
+import '@/assets/styles/css/_colors.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,27 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      // scss: {
+      //   additionalData: `
+      //     @import "@/assets/styles/css/_reset.css";
+      //     @import "@/assets/styles/css/_global.css";
+      //     @import "@/assets/styles/scss/_fonts.scss";
+      //     @import "@/assets/styles/css/_colors.css";
+      //     `
+      //     // @import "@/assets/styles/scss/_break-point.scss";
+      //     // @import "@/assets/styles/scss/_fonts.scss";
+      // }
+      scss: {
+        additionalData: '@use "@/assets/styles/scss/index.scss" as *;'
+      }
     }
   }
 })
